a9e15e931746d4aed9826972713089b9821bfe2d
Patricia AWS <patriciaxiao@g.ucla.edu>
2019-08-01 17:41:34 -0700

    try updating to python3

--- mygraphsage/model.py
+++ mygraphsage/model.py
@@ -12,6 +12,8 @@ from collections import defaultdict
 from encoders import Encoder
 from aggregators import MeanAggregator
 
+import functools
+
 """
 Simple supervised GraphSAGE model as well as examples running the model
 on the Cora and Pubmed datasets.
@@ -46,7 +48,7 @@ def load_cora():
     with open("cora/cora.content") as fp:
         for i,line in enumerate(fp):
             info = line.strip().split()
-            feat_data[i,:] = map(float, info[1:-1])
+            feat_data[i,:] = list(functools.map(float, info[1:-1]))
             node_map[info[0]] = i
             if not info[-1] in label_map:
                 label_map[info[-1]] = len(label_map)
